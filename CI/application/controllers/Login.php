ef91747d997e4513719fe8e65364ec988a15f8fb
Zabdi AG <aguilera.glz.luis@gmail.com>
2015-08-05 14:12:56 -0500

    create branch for zabdi

--- CI/application/controllers/Login.php
+++ CI/application/controllers/Login.php
@@ -5,11 +5,43 @@ class Login extends CI_Controller {
 
 	public function index(){
 		$this->load->helper(array('form'));
-		$this->load->view('header');
-		$this->load->view('view_login');
-		$this->load->view('footer');
+//		$this->load->view('header');
+		$this->load->view('login');
+//		$this->load->view('footer');
 	}
 	public function verifyLogin(){
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('nombre','Nombre','trim|required|xss_clean');
+		$this->form_validation->set_rules('password','Password','trim|required|xss_clean|callback_check_database');
 
+		if($this->form_validation->run() == false){
+			$this->load->view('login_fail');
+		}else{
+			echo "asdasd";
+			//redirect('home');
+		}
 	}
+
+	public function check_database($password){
+		$username = $this->input->post('nombre');
+		$this->load->model('Users','users');
+		$result = $this->users->login($username, $password);
+
+		if($result){
+			$sess_array = array();
+			foreach ($result as $row) {
+				$sess_array = array(
+					'idUsuario' => $row->id,
+					'nombre' => $row->username
+				);
+				$this->session->set_userdata('logged_in', $sess_array);
+			}
+			return true;
+		}else{
+			$this->form_validation->set_message('check_database', 'Usuario o Contrasena incorrectos.');
+			return false;
+		}
+	}
+
 }
--- CI/application/controllers/Verifylogin.php
+++ CI/application/controllers/Verifylogin.php
@@ -2,40 +2,9 @@
 	
  	class Verifylogin extends CI_Controller {
 	
-		public function index(){
-			$this->load->helper('url');
-			$this->load->library('form_validation');
-			$this->form_validation->set_rules('nombre','Nombre','trim|required|xss_clean');
-			$this->form_validation->set_rules('password','Password','trim|required|xss_clean|callback_check_database');
 
-			if($this->form_validation->run() == false){
-				$this->load->view('view_login_fail');
-			}else{
-				echo "asdasd";
-				//redirect('home');
-			}
-		}
 			
-		public function check_database($password){
-			$username = $this->input->post('nombre');
-			$this->load->model('Users','users');
-			$result = $this->users->login($username, $password);
 
-			if($result){
-				$sess_array = array();
-				foreach ($result as $row) {
-					$sess_array = array(
-						'idUsuario' => $row->id,
-						'nombre' => $row->username
-					 );
-					$this->session->set_userdata('logged_in', $sess_array);
-				}
-				return true;
-			}else{
-				$this->form_validation->set_message('check_database', 'Usuario o Contrasena incorrectos.');
-				return false;
-			}
-		}
 	}
 
 ?>
